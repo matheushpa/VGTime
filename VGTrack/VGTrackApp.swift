952e684fc59346a6acea0e0a4d89bb1097b017cb
Matheus Holanda <matheusxiao@gmail.com>
2022-02-20 22:04:05 -0300

    adding initial files.

---
+++ VGTrack/VGTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  VGTrackApp.swift
+//  VGTrack
+//
+//  Created by Matheus Holanda Pinheiro de Albuquerque on 20/02/22.
+//
+
+import SwiftUI
+
+@main
+struct VGTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
